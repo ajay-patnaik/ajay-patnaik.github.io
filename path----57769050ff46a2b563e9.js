808f224627ca5d8652afd9f2bb05f3a6847daad6
Ajay Patnaik <ajayp@nsixty.com>
2018-09-12 22:22:42 -0400

    change background

---
+++ path----57769050ff46a2b563e9.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{105:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Ajay Patnaik"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----57769050ff46a2b563e9.js.map
